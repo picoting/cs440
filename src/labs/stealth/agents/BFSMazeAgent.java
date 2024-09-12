c2efeac6c61736192ac657e335c82d8d962e3b3f
picoting <lindsayl.6857@gmail.com>
2024-09-12 15:27:30 -0400

    finished bfs? now to test....

--- src/labs/stealth/agents/BFSMazeAgent.java
+++ src/labs/stealth/agents/BFSMazeAgent.java
@@ -36,12 +36,20 @@ public class BFSMazeAgent
         int rows = state.getXExtent();
         int cols = state.getYExtent();
 
+        //initialize cardinally adjacent directions
+        int[][] directions = { 
+            {0, -1},
+            {0, 1},
+            {-1, 0},
+            {1, 0}
+        };
+
         //initialize empty queue for BFS and add starting vertex
         Queue<Vertex> q = new LinkedList<Vertex>();
         q.add(src);
 
         //initialize hashmaps for parents, add source
-        Map<Vertex, Set<Vertex>> parents = new HashMap<Vertex, Set<Vertex>>();
+        Map<Vertex, Vertex> parents = new HashMap<Vertex, Vertex>();
         parents.put(src, null);
 
         //intialize set of visited nodes, add source node
@@ -50,9 +58,52 @@ public class BFSMazeAgent
 
         //BFS loop
         while (!q.isEmpty()) {
+            //poll the top vertex on the queue
+            Vertex current = q.poll();
 
+            //check if we have reached target node
+            if (current.equals(goal)) {
+                //initialize null path, start path at goal node
+                Path path = null;
+                Vertex curr = goal;
+
+                //backtrack through parents map
+                while (curr != null) {
+                    Vertex parent = parents.get(curr);
+                    path = new Path(curr, 1, path);
+                    curr = parent;
+                }
+        
+                return path;
+            }
+
+            //check each cardinally adjacent node
+            for (int[] dir: directions) {
+                int newXCoor = current.getXCoordinate() + dir[0];
+                int newYCoor = current.getYCoordinate() + dir[1];
+
+                //ensure the neighboring vertex is on the grid
+                if (state.inBounds(newXCoor, newYCoor)) {
+
+                    //check that there is no resource (tree) there... i think that works....
+                    if (!state.isResourceAt(newXCoor, newYCoor)){
+
+                        //check that the node has not already been discovered
+                        Vertex neighbor = new Vertex(newXCoor, newYCoor);
+                        if (!visited.contains(neighbor)) {
+                            
+                            //add neighbor to queue, parents map, and visited set
+                            q.add(neighbor);
+                            parents.put(neighbor, current);
+                            visited.add(neighbor);
+                        }
+                    }
+                }
+
+            }
         }
 
+        //return null if we never find the goal node
         return null;
     }
 
