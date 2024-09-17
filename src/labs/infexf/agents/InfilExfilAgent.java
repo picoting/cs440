2248b2c8ff087b404722335482dde916c4fd71a2
picoting <lindsayl.6857@gmail.com>
2024-09-17 14:43:04 -0400

    adding lab3 infilexfil stuff

---
+++ src/labs/infexf/agents/InfilExfilAgent.java
@@ -0,0 +1,43 @@
+package src.labs.infexf.agents;
+
+// SYSTEM IMPORTS
+import edu.bu.labs.infexf.agents.SpecOpsAgent;
+import edu.bu.labs.infexf.distance.DistanceMetric;
+import edu.bu.labs.infexf.graph.Vertex;
+import edu.bu.labs.infexf.graph.Path;
+
+
+import edu.cwru.sepia.environment.model.state.State.StateView;
+
+
+// JAVA PROJECT IMPORTS
+
+
+public class InfilExfilAgent
+    extends SpecOpsAgent
+{
+
+    public InfilExfilAgent(int playerNum)
+    {
+        super(playerNum);
+    }
+
+    // if you want to get attack-radius of an enemy, you can do so through the enemy unit's UnitView
+    // Every unit is constructed from an xml schema for that unit's type.
+    // We can lookup the "range" of the unit using the following line of code (assuming we know the id):
+    //     int attackRadius = state.getUnit(enemyUnitID).getTemplateView().getRange();
+    @Override
+    public float getEdgeWeight(Vertex src,
+                               Vertex dst,
+                               StateView state)
+    {
+        return 1f;
+    }
+
+    @Override
+    public boolean shouldReplacePlan(StateView state)
+    {
+        return false;
+    }
+
+}
--- src/labs/stealth/agents/DijkstraMazeAgent.java
+++ src/labs/stealth/agents/DijkstraMazeAgent.java
@@ -13,8 +13,10 @@ import edu.cwru.sepia.util.Direction;                           // Directions in
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.PriorityQueue; // heap in java
+import java.util.Queue;
 import java.util.Set;
 
 
@@ -35,7 +37,68 @@ public class DijkstraMazeAgent
                        Vertex goal,
                        StateView state)
     {
-		return null;
+        //initialize cardinally adjacent directions
+        int[][] directions = { 
+            {0, -1},
+            {0, 1},
+            {-1, 0},
+            {1, 0},
+            {-1, 1},
+            {-1, -1},
+            {1, -1},
+            {1,1}
+        };
+
+        //initialize empty queue for BFS and add starting vertex
+        Queue<Path> q = new LinkedList<Path>();
+        q.add(new Path(src));
+
+        //initialize hashmaps for parents, add source
+        Map<Vertex, Vertex> parents = new HashMap<Vertex, Vertex>();
+        parents.put(src, null);
+
+        System.out.println("finished initializing, starting BFS loop");
+
+        //BFS loop
+        while (!q.isEmpty()) {
+            //poll the top vertex on the queue
+            Path path = q.poll();
+            Vertex current = path.getDestination();
+
+            //check each cardinally adjacent node
+            for (int[] dir: directions) {
+                int newXCoor = current.getXCoordinate() + dir[0];
+                int newYCoor = current.getYCoordinate() + dir[1];
+
+                if (goal.equals(new Vertex(newXCoor, newYCoor))){
+                    return path;
+                }
+
+                //ensure the neighboring vertex is on the grid
+                if (state.inBounds(newXCoor, newYCoor)) {
+
+                    //check that there is no resource (tree) there... i think that works....
+                    if (!state.isResourceAt(newXCoor, newYCoor)){
+
+                        //check that the node has not already been discovered
+                        Vertex neighbor = new Vertex(newXCoor, newYCoor);
+                        if (!parents.containsKey(neighbor)) {
+                            
+                            //add neighbor to queue, parents map, and visited set
+                            q.add(new Path(neighbor, 1f, path));
+                            parents.put(neighbor, current);
+                            //visited.add(neighbor);
+                            //System.out.println("discovered new neighbor");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        //return null if we never find the goal node
+        System.out.println("goal node never reached, returning null");
+        return null;
     }
 
     @Override
